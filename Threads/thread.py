9ee0e808c97e1a3f999b8aff826aa2e9bcb79414
gabrielalmeida-ifpi <126924497+gabrielalmeida-ifpi@users.noreply.github.com>
2023-09-21 19:53:29 -0300

    Update thread.py

--- Threads/thread.py
+++ Threads/thread.py
@@ -1,13 +1,13 @@
 import threading
 import time
 
-def incrementar(n, numero):
+def somar(n, numero):
     for i in range(100):
         print(f'Thread {n}: Contagem: {i + numero}\n')
         time.sleep(2)
 
-thread1 = threading.Thread(target=incrementar, args=(1, 1))
-thread2 = threading.Thread(target=incrementar, args=(2, 3))
+thread1 = threading.Thread(target=somar, args=(1, 1))
+thread2 = threading.Thread(target=somar, args=(2, 3))
 
 thread1.start()
 thread2.start()
